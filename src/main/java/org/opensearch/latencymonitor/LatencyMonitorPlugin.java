c1a7cc0f1e6c060408175d81a64d02116d068fae
Joshua Palis <jpalis@amazon.com>
2022-03-02 21:18:33 +0000

    hooked custom plugin to onIndexModule extension point and added index event listener to handle reaction
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

--- src/main/java/org/opensearch/latencymonitor/LatencyMonitorPlugin.java
+++ src/main/java/org/opensearch/latencymonitor/LatencyMonitorPlugin.java
@@ -6,15 +6,38 @@
  * compatible open source license.
  */
 package org.opensearch.latencymonitor;
+import org.opensearch.OpenSearchException;
+import org.opensearch.index.IndexModule;
+import org.opensearch.index.Index;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.index.shard.IndexEventListener;
 import org.opensearch.plugins.Plugin;
 
 public class LatencyMonitorPlugin extends Plugin {
     
-    // goal : build a new plugin and call this in the critical lifecycle of searching / indexing
-    // approach 
+    // goal : 
+    //      - build a new plugin and call this in the critical lifecycle of searching / indexing
+
+    // approach :
     //      - Identify plugin extension point to leverage. onIndexModule
     //      - onIndexModule contains methods called addIndexEventListener
     //      - addIndexEventListener is a primary extension point for plugins
     //      - using the event listener, I can call beforeIndexCreated / afterIndexCreated
 
+    // hook into plugin extension point
+    @Override
+    public void onIndexModule(IndexModule module) {
+
+        // attatch index event listener to given module
+        module.addIndexEventListener(new IndexEventListener() {
+            
+            // monitor index event creation
+            @Override
+            public void beforeIndexCreated(Index index, Settings indexSettings){
+
+                // Placeholder reaction
+                throw new OpenSearchException("Test : Custom Plugin");
+            }
+        });
+    }
 }
