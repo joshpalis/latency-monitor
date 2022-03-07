4c4d4d90b1d4ae72bda6835338e5c8f77b26771d
Joshua Palis <jpalis@amazon.com>
2022-03-07 20:34:39 +0000

    fixed 10_basic.tml match and added RunPlugin java file
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

--- src/main/java/org/opensearch/latencytester/plugin/RunPlugin.java
+++ src/main/java/org/opensearch/latencytester/plugin/RunPlugin.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.opensearch.latencytester.plugin;
+
 public class RunPlugin {
     
+    public static void main(String[] args) {
+        System.out.println("TESTTTTTTTTTTTTTTTTTTT");
+        LatencyTesterPlugin latencyPlugin = new LatencyTesterPlugin();
+    }
 }
